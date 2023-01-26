5b154b588537af29a12e29ef65d15cb533e5e71b
Alex Braga <bragadeveloper@gmail.com>
2023-01-26 15:14:42 -0300

    header do perfil

--- src/app/shared/components/admin/admin-shared.module.ts
+++ src/app/shared/components/admin/admin-shared.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
     BrowserModule,
     CommonModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ]
 })
 export class AdminSharedModule { }
